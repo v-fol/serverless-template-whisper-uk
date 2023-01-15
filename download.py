c5d2a90c46c6587977068009239407cad113a072
Vova Foliush <72522652+v-fol@users.noreply.github.com>
2023-01-15 18:08:20 +0200

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ import whisper
 import torch
 
 def download_model():
-    model = whisper.load_model("base")
+    model = whisper.load_model("large")
 
 if __name__ == "__main__":
     download_model()
